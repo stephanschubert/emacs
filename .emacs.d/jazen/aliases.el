6db05af2ea6fe3cf99ffbff99588e06a22f06a0b
jazen <stephan@frozencherry.de>
2009-05-06 23:41:30 +0200

    Some more bindings and a new alias

---
+++ .emacs.d/jazen/aliases.el
@@ -0,0 +1,1 @@
+(defalias 'qrr 'query-replace-regexp)
--- .emacs.d/jazen/bindings.el
+++ .emacs.d/jazen/bindings.el
@@ -8,3 +8,11 @@
      (define-key shell-mode-map [down] 'comint-next-input)
      (define-key shell-mode-map "\C-p" 'comint-previous-input)
      (define-key shell-mode-map "\C-n" 'comint-next-input)))
+
+; Invoke M-x without the Alt key - Note that i mapped CapsLock
+; as another Control key using ~/.Xresources
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+
+; Prefer backward-kill-word over Backspace
+(global-set-key "\C-w" 'backward-kill-word) ; was kill-region
+(global-set-key "\C-x\C-k" 'kill-region)    
