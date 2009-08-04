b08fa179595cb0d99ee9dab35d09494166a1a522
jazen <stephan@frozencherry.de>
2009-08-04 12:38:45 +0200

    Updated and moved cucumber/feature-mode.

--- .emacs.d/jazen/cucumber.el
+++ .emacs.d/jazen/cucumber.el
@@ -1,2 +1,3 @@
-(add-to-list 'load-path "~/.emacs.d/vendor/cucumber-mode")
+(add-to-list 'load-path "~/.emacs.d/vendor/feature-mode")
 (autoload 'feature-mode "feature-mode" "Mode for editing cucumber files" t)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
