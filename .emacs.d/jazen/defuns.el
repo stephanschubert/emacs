5f2183d83715d7bbdebf13d48e6b23f0fa53b7ab
jazen <stephan@frozencherry.de>
2009-11-13 19:22:41 +0100

    [FEATURE] Added advices to copy/cut the current line without the need for selection first.

--- .emacs.d/jazen/defuns.el
+++ .emacs.d/jazen/defuns.el
@@ -103,3 +103,24 @@
         (isearch-forward regexp-p no-recursive-edit)))))
 
 (global-set-key (kbd "M-s") 'isearch-forward-at-point)
+
+
+;; ------------------------------------------------------------------------------
+;; It is assumed that Transient Mark Mode is enabled globally
+;; (transient-mark-mode t)
+;; -----------------------------------------------------------------------------
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
