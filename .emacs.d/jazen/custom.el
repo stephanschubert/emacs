159347c403ffe26e77f4e254f9ea7fab3b22284a
jazen <stephan@frozencherry.de>
2009-12-01 07:43:07 +0100

    Added custom uniqification for buffer names

--- .emacs.d/jazen/custom.el
+++ .emacs.d/jazen/custom.el
@@ -4,3 +4,13 @@
 
 (custom-set-faces
 )
+
+; Unique buffer naming -
+; If two or more files have the same name but are in different
+; directories, this will present them as "foo/dir-a" and 
+; "foo/dir-b" which is more convenient than "foo" and "foo<2>".
+
+(require 'uniquify) 
+(setq 
+  uniquify-buffer-name-style 'post-forward
+  uniquify-separator "/")
