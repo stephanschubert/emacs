b07397981ea3ee1779cafb9b4695696fcaf6b11b
jazen <jazen@vm1.(none)>
2010-05-09 11:10:43 +0200

    Add jzn/delete-trailing-blank-lines to the before-save-hook to keep files clean

--- .emacs.d/jazen/defuns.el
+++ .emacs.d/jazen/defuns.el
@@ -45,6 +45,20 @@
 	(delete-window))
 (global-set-key (kbd "C-c k") 'jzn/close-other-window)
 
+;; Clear up files before saving them -------------------------------------------
+
+(defun jzn/delete-trailing-blank-lines ()
+  "Deletes all blank lines at the end of the file and leaves single 
+   newline character."
+  (interactive)
+  (save-excursion
+    (goto-char (point-max))
+    (newline)              ;; ensures that there is at least one
+    (delete-blank-lines))) ;; leaves at most one
+
+(add-hook 'before-save-hook 'jzn/delete-trailing-whitespace)
+(add-hook 'before-save-hook 'jzn/delete-trailing-blank-lines)
+
 ; ------------------------------------------------------------------------------
 
 (defun rename-file-and-buffer (new-name)
