3b17ab032cbc173cac6ca2e6fa0b7657f823daa9
Stephan Schubert <stephan@frozencherry.de>
2013-06-12 09:25:42 +0200

    Add coffee-mode

---
+++ .emacs.d/jazen/coffeescript.el
@@ -0,0 +1,7 @@
+(vendor 'coffee-mode)
+
+;; Automatically clean up bad whitespace
+(setq whitespace-action '(auto-cleanup))
+
+;; Only show bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab))
