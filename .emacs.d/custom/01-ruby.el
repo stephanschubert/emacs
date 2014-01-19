9caa04b36bb9fc1fc0cd9ee7502f2211cda1b9fb
Stephan Schubert <stephan@frozencherry.de>
2014-01-19 17:42:38 +0100

    Add get-closest-gemfile-root, rspec-compile-file and rspec-compile-on-line functions to enhanced-ruby mode.

--- .emacs.d/custom/01-ruby.el
+++ .emacs.d/custom/01-ruby.el
@@ -11,3 +11,38 @@
 
 (setq enh-ruby-bounce-deep-indent t)
 (setq enh-ruby-hanging-brace-indent-level 2)
+
+(defun* get-closest-gemfile-root (&optional (file "Gemfile"))
+  "Determine the pathname of the first instance of FILE starting from the current directory
+   towards root. This may not do the correct thing in presence of links. If it does not find FILE,
+   then it shall return the name of FILE in the current directory, suitable for creation"
+  (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
+    (loop
+     for d = default-directory then (expand-file-name ".." d)
+     if (file-exists-p (expand-file-name file d))
+     return d
+     if (equal d root)
+     return nil)))
+
+(require 'compile)
+
+(defun rspec-compile-file ()
+  (interactive)
+  (compile (format "cd %s; bundle exec rspec %s"
+                   (get-closest-gemfile-root)
+                   (file-relative-name (buffer-file-name) (get-closest-gemfile-root))
+                   ) t))
+
+(defun rspec-compile-on-line ()
+  (interactive)
+  (compile (format "cd %s; bundle exec rspec %s -l %s"
+                   (get-closest-gemfile-root)
+                   (file-relative-name (buffer-file-name) (get-closest-gemfile-root))
+                   (line-number-at-pos)
+                   ) t))
+
+(add-hook 'enh-ruby-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c l") 'rspec-compile-on-line)
+            (local-set-key (kbd "C-c k") 'rspec-compile-file)
+            ))
