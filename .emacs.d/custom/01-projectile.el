c5fb79f539293ec1b5ef4a79836dfb3694b50489
Stephan Schubert <stephan@frozencherry.de>
2014-01-19 18:54:05 +0100

    Deactivate s-p and s-b keys because i haven't figured out a way to use Apple's Command key as Super key when running Emacs in iTerm2.

--- .emacs.d/custom/01-projectile.el
+++ .emacs.d/custom/01-projectile.el
@@ -4,7 +4,7 @@
 (setq projectile-completion-system 'ido)
 
 ;; Press Command-p for fuzzy find in project
-(global-set-key (kbd "s-p") 'projectile-find-file)
+;;(global-set-key (kbd "s-p") 'projectile-find-file)
 
 ;; Press Command-b for fuzzy switch buffer
-(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+;;(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
