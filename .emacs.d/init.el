1c618968e87683a428db8bf3f96026f382f03570
Stephan Schubert <stephan@frozencherry.de>
2014-01-20 07:43:19 +0100

    Add coffeescript mode. And minor tweaks.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,52 +13,38 @@
 (load "01-ruby")
 (load "01-smartparens")
 
-
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor")
 (add-to-list 'load-path "~/.emacs.d/vendor/egg")
 
-; Explicit loading without load-path adjustment because
-; some names could clash with other libraries.
+;; Explicit loading without load-path adjustment because
+;; some names could clash with other libraries.
 
 (load "jazen/defuns")
 (load "jazen/bindings")
+(load "jazen/coffeescript")
 (load "jazen/custom")
 (load "jazen/osx")
-;;(load "jazen/global")
+;; (load "jazen/global")
 (load "jazen/shell")
 (load "jazen/ruby")
 (load "jazen/rails")
 (load "jazen/yaml")
 (load "jazen/haml")
 (load "jazen/sass")
-(load "jazen/theme")
-;(load "jazen/icicles") Broken?
+;; (load "jazen/icicles") Broken?
 (load "jazen/mustache")
 (load "jazen/scss")
 (load "jazen/textile")
-;(load "jazen/jabber") Obsolete?
 (load "jazen/yasnippet")
-;(load "jazen/cucumber") Obsolete?
 (load "jazen/nxml")
-;(load "jazen/javascript") Broken?
 (load "jazen/restclient")
 (load "jazen/window-number")
 
 (vendor 'egg)
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-
-; Always run in server mode
+;; Always run in server mode
 (if (not (server-mode)) (server-start))
+
+;; Got some issues so will be loaded last so it wont interfere with other stuff.
+(load "jazen/theme")
