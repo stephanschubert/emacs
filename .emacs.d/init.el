5b465478041e219c195ce807c7bca54ebd30860b
Stephan Schubert <stephan@frozencherry.de>
2014-01-28 08:25:25 +0100

    Minor auto-complete update and configuration tweak.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,3 +48,15 @@
 
 ;; Got some issues so will be loaded last so it wont interfere with other stuff.
 (load "jazen/theme")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js-indent-level 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
