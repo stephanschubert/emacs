c4a8b3d366bfa9087dc39de56d554ca37c6439cd
Stephan Schubert <stephan@frozencherry.de>
2014-03-27 12:04:51 +0100

    Add powerline.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,7 @@
 (load "01-ido")
 (load "01-highlight-indentation")
 (load "01-magit")
+(load "01-powerline")
 (load "01-projectile")
 (load "01-ruby")
 (load "01-smartparens")
@@ -50,10 +51,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js-indent-level 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(js-indent-level 2)
+ '(powerline-default-separator (quote arrow)))
