9d01f35af0c6c37224568aa925cecfbfdacdb66f
Stephan Schubert <stephan@frozencherry.de>
2014-06-19 12:21:32 +0200

    ace-jump: Use C-j instead of C-c SPC.

--- .emacs.d/custom/01-ace-jump-mode.el
+++ .emacs.d/custom/01-ace-jump-mode.el
@@ -4,7 +4,8 @@
   "Emacs quick move minor mode"
   t)
 
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-j") 'ace-jump-mode)
 
 ; Enable a more powerful jump back function from ace jump mode
 (autoload
