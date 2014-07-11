a13b74fd297adfdcf2f5d8a361a842e61c8e7e52
Stephan Schubert <stephan@frozencherry.de>
2014-07-11 14:39:20 +0200

    Mode line: Put line:column numbers first.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -56,4 +56,67 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js-indent-level 2)
+ '(mode-line-format
+   (quote ("%e"
+ (:eval
+  (let*
+      ((active
+        (powerline-selected-window-active))
+       (mode-line
+        (if active 'mode-line 'mode-line-inactive))
+       (face1
+        (if active 'powerline-active1 'powerline-inactive1))
+       (face2
+        (if active 'powerline-active2 'powerline-inactive2))
+       (separator-left
+        (intern
+         (format "powerline-%s-%s" powerline-default-separator
+                 (car powerline-default-separator-dir))))
+       (separator-right
+        (intern
+         (format "powerline-%s-%s" powerline-default-separator
+                 (cdr powerline-default-separator-dir))))
+       (lhs
+        (list
+         (powerline-raw "%l:%c " face1 'l)
+         (powerline-raw "%*" nil 'l)
+         (powerline-buffer-size nil 'l)
+         (powerline-raw mode-line-mule-info nil 'l)
+         (powerline-buffer-id nil 'l)
+         (when
+             (and
+              (boundp 'which-func-mode)
+              which-func-mode)
+           (powerline-raw which-func-format nil 'l))
+         (powerline-raw " ")
+         (funcall separator-left mode-line face1)
+         (when
+             (boundp 'erc-modified-channels-object)
+           (powerline-raw erc-modified-channels-object face1 'l))
+         (powerline-major-mode face1 'l)
+         (powerline-process face1)
+         (powerline-minor-modes face1 'l)
+         (powerline-narrow face1 'l)
+         (powerline-raw " " face1)
+         (funcall separator-left face1 face2)
+         (powerline-vc face2 'r)))
+       (rhs
+        (list
+         (powerline-raw global-mode-string face2 'r)
+         (funcall separator-right face2 face1)
+         (funcall separator-right face1 mode-line)
+         (powerline-raw " ")
+         (powerline-raw "%6p" nil 'r)
+         (powerline-hud face2 face1))))
+    (concat
+     (powerline-render lhs)
+     (powerline-fill face2
+                     (powerline-width rhs))
+     (powerline-render rhs)))))))
  '(powerline-default-separator (quote arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
