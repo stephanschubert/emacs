613f90af5f08e67a60317beddea7dd7ab18bb87e
Stephan Schubert <stephan@frozencherry.de>
2014-11-24 07:32:22 +0100

    functions: Introduce jzn/toggle-quotes bound to C-c C-t.

---
+++ .emacs.d/custom/00-functions.el
@@ -0,0 +1,30 @@
+(defun jzn/toggle-quotes ()
+  "Toggle single quoted string to double or vice versa, and
+  flip the internal quotes as well.  Best to run on the first
+  character of the string."
+  (interactive)
+  (save-excursion
+    (let ((start (nth 8 (syntax-ppss)))
+          (quote-length 0) sub kind replacement)
+      (goto-char start)
+      (setq sub (buffer-substring start (progn (forward-sexp) (point)))
+            kind (aref sub 0))
+      (while (char-equal kind (aref sub 0))
+        (setq sub (substring sub 1)
+              quote-length (1+ quote-length)))
+      (setq sub (substring sub 0 (- (length sub) quote-length)))
+      (goto-char start)
+      (delete-region start (+ start (* 2 quote-length) (length sub)))
+      (setq kind (if (char-equal kind ?\") ?\' ?\"))
+      (loop for i from 0
+            for c across sub
+            for slash = (char-equal c ?\\)
+            then (if (and (not slash) (char-equal c ?\\)) t nil) do
+            (unless slash
+              (when (member c '(?\" ?\'))
+                (aset sub i
+                      (if (char-equal kind ?\") ?\' ?\")))))
+      (setq replacement (make-string quote-length kind))
+      (insert replacement sub replacement))))
+
+(global-set-key (kbd "C-c C-t") 'jzn/toggle-quotes)
