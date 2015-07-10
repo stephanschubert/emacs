22a6043aaa70d0ba2078168fdc39f99eccd78368
Stephan Schubert <stephan@frozencherry.de>
2015-07-10 09:13:14 +0200

    helm: Bind C-x C-f to helm-find-files and use C-h a for helm-apropos with fuzzy matching.

--- .emacs.d/custom/01-helm.el
+++ .emacs.d/custom/01-helm.el
@@ -1,9 +1,20 @@
 (require 'helm-config)
 (helm-mode 1)
 
+;; Enable ivy-mode from swiper(-helm)
+(ivy-mode t)
+
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-h a") 'helm-apropos)
 
-; Enable ivy-mode from swiper(-helm)
-(ivy-mode t)
+(setq helm-apropos-fuzzy-match t)
+
+(setq helm-ff-skip-boring-files t)
+(setq helm-boring-file-regexp-list
+  '("\\.#" "\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "~$"
+    "\\.so$" "\\.a$" "\\.elc$" "\\.fas$" "\\.fasl$" "\\.pyc$" "\\.pyo$"))
+(setq helm-boring-buffer-regexp-list
+  '("\\` " "\\*helm" "\\*helm-mode" "\\*Echo Area" "\\*tramp" "\\*Minibuf" "\\*epc"))
