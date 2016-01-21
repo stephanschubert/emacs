73ade5ec032d04114caa7c559b6ac3bae676677f
Stephan Schubert <stephan@frozencherry.de>
2016-01-21 09:58:33 +0100

    window-number: Remove obsolete vendor'd version and tweak the rendering in the mode-line.

--- .emacs.d/custom/01-window-number.el
+++ .emacs.d/custom/01-window-number.el
@@ -1,2 +1,11 @@
 (require 'window-number)
 (window-number-mode 1)
+
+;; The first 'empty' string contains a regular space and a 'thin space'
+;; and the second 'empty' string contains an 'en space'.
+(defun window-number-string ()
+  "Returns the string containing the number of the current window"
+  (propertize
+   (concat "  " (number-to-string (window-number)) " ")
+   'face
+   'window-number-face))
