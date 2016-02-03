7d61cd915113b27f69d6b2d868dd62d8a58052ea
Stephan Schubert <stephan@frozencherry.de>
2016-02-03 13:55:11 +0100

    key-combo: Enable coffee-mode too.

--- .emacs.d/custom/01-key-combo.el
+++ .emacs.d/custom/01-key-combo.el
@@ -1,2 +1,6 @@
 (global-key-combo-mode t)
+
+;; JS modes are enabled by default - let's add coffee-mode too..
+(add-to-list 'key-combo-common-mode-hooks 'coffee-mode-hook)
+
 (key-combo-load-default)
