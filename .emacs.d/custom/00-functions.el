15d025161b54d6b3320269304a54ee3554251c83
Stephan Schubert <stephan@frozencherry.de>
2016-02-19 12:30:42 +0100

    Advice `server-visit-files` to understand file:line[:column] pattern.

--- .emacs.d/custom/00-functions.el
+++ .emacs.d/custom/00-functions.el
@@ -56,3 +56,18 @@
             (mapconcat 'isearch-text-char-description isearch-new-string ""))))
 
 (define-key isearch-mode-map (kbd "DEL") 'jzn/isearch-delete)
+
+;; Details https://stackoverflow.com/a/23857738
+(defadvice server-visit-files (before parse-numbers-in-lines (files proc &optional nowait) activate)
+  "Look for filenames like file:line or file:line:column and re-parses the name in such manner that position in file."
+  (ad-set-arg 0
+              (mapcar (lambda (fn)
+                        (let ((name (car fn)))
+                          (if (string-match "^\\(.*?\\):\\([0-9]+\\)\\(?::\\([0-9]+\\)\\)?$" name)
+                              (cons
+                               (match-string 1 name)
+                               (cons (string-to-number (match-string 2 name))
+                                     (string-to-number (or (match-string 3 name) "")))
+                               )
+                            fn)))
+                      files)))
