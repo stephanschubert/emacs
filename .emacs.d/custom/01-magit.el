d67ba729a249095bb4e5c28946130b69bce088cd
Stephan Schubert <stephan@frozencherry.de>
2016-03-17 15:58:09 +0100

    magit: Bind 'endless/visit-pull-request-url to 'v' in mode-map

--- .emacs.d/custom/01-magit.el
+++ .emacs.d/custom/01-magit.el
@@ -3,3 +3,20 @@
 
 ; Use ivy for completion
 (setq magit-completing-read-function 'ivy-completing-read)
+
+;; http://endlessparentheses.com/create-github-prs-from-emacs-with-magit.html
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/pull/new/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-push-remote)
+                       "url"))
+           (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "v"
+     #'endless/visit-pull-request-url))
